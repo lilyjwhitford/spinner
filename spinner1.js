118530968ac7b5f4e4d3b7329ffe1490df05a185
Lily Whitford <lilywhitford2@gmail.com>
2024-02-15 23:09:14 +0000

    made little spinner animation

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 110);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 130);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 150);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 170);
+
+
